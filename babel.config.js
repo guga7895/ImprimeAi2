3d914f3b8bc1c7c28cbe2dfaa808ea9a6027c32b
guga7895 <guga7894@gmail.com>
2024-11-03 18:01:03 -0300

    Arrumando configurações de inicialização.

--- babel.config.js
+++ babel.config.js
@@ -12,5 +12,8 @@ module.exports = {
       moduleName: '@env',
       path: '.env',
     }],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }]
   ],
 };
